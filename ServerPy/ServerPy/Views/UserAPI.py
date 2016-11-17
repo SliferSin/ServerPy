9081f6cedc244ba61c8c38e414c9236c276f27ea
LaptopSly <sky_dragoon86@yahoo.es>
2016-11-17 09:58:55 +0100

    GetInfo fet, falta prova

--- ServerPy/ServerPy/Views/UserAPI.py
+++ ServerPy/ServerPy/Views/UserAPI.py
@@ -12,7 +12,7 @@ def AddUser():
         if request.headers['Content-Type'] == 'application/json':
             usuario = request.json
             u = DBClass.User(usuario)
-            c.execute("INSERT INTO Usuari VALUES (?,?,?,?,?)",[u.DNI,u.name,u.lastname,u.edat,u.sexe])
+            c.execute("INSERT INTO Usuari VALUES (?,?,?,?,?)",[u.DNI,u.name,u.lastname,u.age,u.gender])
             conn.commit()    
     except sqlite3.Error as e:
         print("Error: ",e.args[0])
@@ -40,16 +40,24 @@ def DelUser():
     conn.close()        
     return "Fila eliminada"
 
-#@app.route('/User/<userID>',methods = ['GET']) #Consultar usuari
-## @Return: DNI,Nom,Cognom,edat from Usuari
-## @Return: SensorID from Sensor
-## @Return: Dades,data from Dades
-#def GetUser(userID):
-#    conn = sqlite3.connect('IS.db')
-#    c = conn.cursor()
-#    try:
-#        if request.headers['Content-Type'] == 'application/json':
-#        #IDusuario = userID # se podrá obviar seguramente
+@app.route('/User/GetInfo',methods = ['GET']) #Consultar usuari
+def GetUser():
+    conn = sqlite3.connect('IS.db')
+    c = conn.cursor()
+    try:
+        if request.headers['Content-Type'] == 'application/json':
+            uDNI = request.args.get('DNI')
+            c.execute("SELECT u.DNI,u.Nom,u.Cognom,u.Edat,d.X,d.Y,d.Z,d.data,d.ID_Sensor FROM Usuari u, Dades d WHERE u.DNI = ?",[uDNI])
+            info_usuari = c.fetchone()
+            info = DBClass.GetUser(info_usuari)
+            resp = info.Send()
+            
+        
+    except sqlite3.Error as e:
+        print("Error:",e.args[0])
+    
+    conn.close()
+    return resp
            
 
         
