caa16dbe64e41f74159add4bdc60b81115162528
LaptopSly <sky_dragoon86@yahoo.es>
2016-11-24 10:00:24 +0100

    Web login, error registro

--- ServerPy/ServerPy/Login.py
+++ ServerPy/ServerPy/Login.py
@@ -1,18 +1,37 @@
 import sqlite3
 import DBClass
-from flask import Flask,request,Response,session
+from flask import Flask,request,Response,session,redirect,url_for,render_template
 from ServerPy import app 
 
-@app.route("/login", methods = ['GET','POST']) #Loggin or Register
+#localhost:155/login?username="loquesea"&password="loquesea"
+@app.route("/login", methods = ['GET','POST']) #Login or Register
 def Login():
      if request.method == 'GET':
         username = request.args.get('username')
         password = request.args.get('password')
         loguser = DBClass.LogUser(username,password)
-        try:
-            if loguser.VerifyLogin(): 
-                session['username'] = username
+        
+        if loguser.VerifyLogin(): 
+            session['username'] = username
+            return redirect(url_for('index'))
+        else:
+            return "Registrat com " + username + "<br>"
 
      else : #Registrar usuario
-        session['username'] = request.arg.get('username')
+        return redirect(url_for('Register'))        
         
+@app.route('/logout')
+def logout():
+   # remove the username from the session if it is there
+   session.pop('username', None)
+   return redirect(url_for('index'))
+
+@app.route('/register')
+def Register():
+    return redirect(url_for('/Login'))
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('log.html')
+    
--- ServerPy/ServerPy/__init__.py
+++ ServerPy/ServerPy/__init__.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
 import os
 
 app = Flask(__name__)
 
+#Secret Key necesitada pel objecte session
 app.secret_key = os.urandom(24)
 
 import ServerPy.Views.DadesAPI
 import ServerPy.Views.SensorAPI
 import ServerPy.Views.UserAPI
+import ServerPy.Login
+import ServerPy.Templates
 import DBClass
